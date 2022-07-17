1b53b5dc94b0bccdc95c9a9d8526bfb12c999d42
Richard Azia <richard@main-vision.com>
2022-07-17 18:46:17 +0200

    factorial

--- factorial.rb
+++ factorial.rb
@@ -4,3 +4,29 @@ def factorial_value_sum_generator(factorial)
 end
 
 p factorial_value_sum_generator(100)
+
+class Fibonacci2
+	def initialize(max)
+		@num_1 = 0
+		@i = 0
+		@sum = 0
+		@num_2 = 1
+		@max = max
+	end
+
+	def even_fibonacci
+		while @i <= @max
+			@i = @num_1 + @num_2
+
+			@sum += @i if @i if @i % 2 == 0
+
+			@num_1 = @num_2
+
+			@num_2 = @i
+		end
+		@sum
+	end
+end
+
+result = Fibonacci2.new(4_000_000)
+p result.even_fibonacci
